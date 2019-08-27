80c1f3b1c3ce82fca529f3ec72e856e363f4d736
Aslijia <medivh.wd@gamail.com>
2019-08-27 11:52:37 +0800

    use single connect

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { Client, ConfigOptions } from "elasticsearch";
 import { BulkWriter } from "./bulk_writer";
 
-
 declare interface Configure {
     host: string,
     pipeline?: string,
@@ -10,25 +9,30 @@ declare interface Configure {
     interval?: number
 }
 
+let bulk: BulkWriter | undefined;
+
 export function configure(cfg: Configure) {
     const opts: ConfigOptions = {
         host: cfg.host || 'http://localhost:9200',
         keepAlive: true
     };
-    const bulk = new BulkWriter(new Client(opts), {
-        pipeline: cfg.pipeline,
-        buffering: cfg.bulk ? true : false,
-        bufferLimit: cfg.bulk,
-        waitForActiveShards: cfg.waitForActiveShards,
-        interval: cfg.interval
-    });
-    bulk.start();
-
+    if (!bulk) {
+        bulk = new BulkWriter(new Client(opts), {
+            pipeline: cfg.pipeline,
+            buffering: cfg.bulk ? true : false,
+            bufferLimit: cfg.bulk,
+            waitForActiveShards: cfg.waitForActiveShards,
+            interval: cfg.interval
+        });
+        bulk.start();
+    }
     return function (event: any) {
         const body = event.data[1] ? event.data[1] : {};
         body.message = event.data[0];
-        bulk.append(event.categoryName, 'doc', {
-            level: event.level.levelStr, category: event.categoryName, timestamp: Date.now(), body
-        });
+        if (bulk) {
+            bulk.append(event.categoryName, 'doc', {
+                level: event.level.levelStr, category: event.categoryName, timestamp: Date.now(), body
+            });
+        }
     }
 }
