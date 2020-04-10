49ecf459d91f1a2cee7ccff34f2bdcce28b3cd90
Aslijia <medivh.wd@gamail.com>
2020-04-10 14:39:57 +0800

    resolve data

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import { Client, ClientOptions } from "@elastic/elasticsearch";
-import { isObject, isUndefined, isArray, isNull, isNumber } from "util";
+import { isObject, isUndefined, isArray, isNull } from "util";
 
 let client: Client;
 
-function resolve(o: any) {
+function resolve(o: any, deep: number) {
+
     for (let i in o) {
         if (isObject(o[i])) {
-            o[i] = resolve(o[i]);
+            if (deep)
+                o[i] = resolve(o[i], deep--);
+            else
+                o[i] = JSON.stringify(o[i]);
+
         } else if (isUndefined(o[i]) || isNull(o[i])) {
             delete o[i];
+        } else if (isArray(o[i])) {
+            o[i] = JSON.stringify(o[i]);
         }
     }
     return o;
@@ -21,7 +28,7 @@ export function configure(cfg: ClientOptions) {
 
     return function (event: any) {
         const message = event.data[0];
-        const context = resolve(isObject(event.data[1]) ? event.data[1] : {});
+        const context = resolve(isObject(event.data[1]) ? event.data[1] : {}, 3);
 
         client.index({
             index: event.categoryName,
@@ -40,7 +47,24 @@ export function configure(cfg: ClientOptions) {
                 pid: event.pid
             }
         }).catch(err => {
-            console.error('lo4js append logger to elasticsearch failed', { event, message: err.message });
+            client.index({
+                index: 'exception',
+                type: 'logger',
+                body: {
+                    time: new Date().toISOString(),
+                    error: err.message,
+                    category: event.categoryName,
+                    level: event.level.levelStr.toLocaleLowerCase(),
+                    message,
+                    context: JSON.stringify(context),
+                    trace: {
+                        filename: event.data[2] || 'unknown',
+                        function: event.data[3] || 'unknown',
+                        line: event.data[4] || -1
+                    },
+                    pid: event.pid
+                }
+            });
         });
     }
 }
