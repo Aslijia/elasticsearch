87159bdfecd1ba6c08fa9989f0ed5b6fb4d7d9e8
jason <jason@gmail.com>
2020-11-04 15:43:00 +0800

    addlogs

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,38 @@
-import { Client, ClientOptions } from "@elastic/elasticsearch";
+import { Client, ClientOptions } from '@elastic/elasticsearch'
 
-let client: Client;
+let client: Client
 
 export function configure(cfg: ClientOptions) {
-  if (!client) {
-    client = new Client(cfg);
-  }
+	if (!client) {
+		client = new Client(cfg)
+	}
 
-  return function (event: any) {
-    const message = event.data[0];
-    const context = event.data[1] || {};
-    client
-      .index({
-        index: event.categoryName,
-        type: "log4js",
-        body: {
-          time: event.startTime,
-          category: event.categoryName,
-          level: event.level.levelStr.toLocaleLowerCase(),
-          message,
-          context,
-          trace: {
-            filename: event.data[2] || "unknown",
-            function: event.data[3] || "unknown",
-            line: event.data[4] || -1,
-          },
-          pid: event.pid,
-        },
-      })
-      .catch((err) => {
-        console.error("error", {
-          err: err.meta,
-          category: event.categoryName,
-          message,
-        });
-      });
-  };
+	return function (event: any) {
+		const message = event.data[0]
+		const context = event.data[1] || {}
+		client
+			.index({
+				index: event.categoryName,
+				type: 'log4js',
+				body: {
+					time: event.startTime,
+					category: event.categoryName,
+					level: event.level.levelStr.toLocaleLowerCase(),
+					message,
+					context,
+					trace: {
+						filename: event.data[2] || 'unknown',
+						function: event.data[3] || 'unknown',
+						line: event.data[4] || -1,
+					},
+					pid: event.pid,
+				},
+			})
+			.catch((err) => {
+				console.error('elasticsearch write failed:', {
+					message: err.meta.body.error,
+					category: `${event.categoryName}[${message}]`,
+				})
+			})
+	}
 }
