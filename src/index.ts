59d574de69a086e0693037dc21a8b450c5fb88e0
jason <jason@gmail.com>
2020-11-19 15:35:35 +0800

    update submodule

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,54 @@
 import { Client, ClientOptions } from '@elastic/elasticsearch'
 
-let client: Client
+let client: Client | undefined
+
+declare interface Content {
+	categoryName: string
+	startTime: string
+	level: {
+		levelStr: string
+	}
+	data: [string, any, string?, string?, string?]
+	pid: string
+}
+
+function post2els(content: Content) {
+	if (!client) {
+		throw new Error(`cant write data 2 elasticsearch`)
+	}
+	client
+		.index({
+			index: content.categoryName,
+			type: 'log4js',
+			body: {
+				time: content.startTime,
+				category: content.categoryName,
+				level: content.level.levelStr,
+				message: content.data[0],
+				context: content.data[1],
+				trace: {
+					filename: content.data[2] || '-',
+					function: content.data[3] || '-',
+					line: content.data[4] || -1,
+				},
+				pid: content.pid,
+			},
+		})
+		.catch((err) => {
+			post2els({
+				categoryName: 'unplanned',
+				startTime: content.startTime,
+				level: content.level,
+				data: [content.data[0], err.meta],
+				pid: content.pid,
+			})
+			console.error(err.meta)
+		})
+}
 
 export function configure(cfg: ClientOptions) {
 	if (!client) {
 		client = new Client(cfg)
 	}
-
-	const els = function (event: any) {
-		const message = event.data[0]
-		const context = event.data[1] || {}
-		client
-			.index({
-				index: event.categoryName,
-				type: 'log4js',
-				body: {
-					time: event.startTime,
-					category: event.categoryName,
-					level: event.level.levelStr.toLocaleLowerCase(),
-					message,
-					context,
-					trace: {
-						filename: event.data[2] || '-',
-						function: event.data[3] || '-',
-						line: event.data[4] || -1,
-					},
-					pid: event.pid,
-				},
-			})
-			.catch((err) => {
-				els({
-					categoryName: 'elasticsearch',
-					startTime: event.startTime,
-					pid: event.pid,
-					level: event.level,
-					data: ['elasticsearch write failed', err.meta],
-				})
-				console.error(
-					'elasticsearch write failed',
-					event.categoryName,
-					message,
-					err.meta
-				)
-			})
-	}
-	return els
+	return post2els
 }
